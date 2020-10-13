56bf9ffb2ca67f0dabbed2d96dce4bf849f37052
cherrrnikov <chernikoviam@gmail.com>
2020-10-13 22:47:06 +0300

    create advantages section with adaptive

--- js/main.js
+++ js/main.js
@@ -4,4 +4,8 @@ $(document).ready(function () {
   menuButton.on("click", function() {
     $(".navbar-window").toggleClass("navbar-window--visible");
   })
+  var menuLink = $(".navbar-menu__link")
+  menuLink.on("click", function() {
+    $(".navbar-window").removeClass("navbar-window--visible");
+  })
 })
