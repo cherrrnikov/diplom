48da8d52a4e38c7459f1cc714df53ff836a9662e
cherrrnikov <chernikoviam@gmail.com>
2020-10-14 23:40:41 +0300

    create trends section with tabs and adaptive

--- js/main.js
+++ js/main.js
@@ -8,4 +8,51 @@ $(document).ready(function () {
   menuLink.on("click", function() {
     $(".navbar-window").removeClass("navbar-window--visible");
   })
+
+
+  // Переключение по табам
+  // Таб "Новинки"
+  var trendsTabNew = $(".trends-tabs__tab--primary")
+  trendsTabNew.on("click", function() {
+    $(".trends-card").removeClass("trends-card--hidden");
+    $(".trends-button").removeClass("trends-button--hidden");
+    $(".best-card").removeClass("best-card--visible");
+    $(".soon-card").removeClass("soon-card--visible")
+
+  // Меняются цвета табов при клике
+    $(".trends-tabs__tab--primary").removeClass("news-tab");
+    $(".trends-tabs__tab--best").removeClass("best-tab");
+    $(".trends-tabs__tab--soon").removeClass("soon-tab")
+  })
+  
+  // Таб "Лучшее"
+  var trendsTabBest = $(".trends-tabs__tab--best")
+  trendsTabBest.on('click', function() {
+    $('.trends-card').addClass("trends-card--hidden");
+    $(".trends-button").addClass("trends-button--hidden");
+    $(".best-card").addClass("best-card--visible");
+    $(".soon-card").removeClass("soon-card--visible")
+    
+
+  // Меняются цвета табов при клике
+    $(".trends-tabs__tab--primary").addClass("news-tab");
+    $(".trends-tabs__tab--best").addClass("best-tab");
+    $(".trends-tabs__tab--soon").removeClass("soon-tab")
+  
+  })
+  
+  // Таб "Скоро"
+  var trendsTabSoon = $(".trends-tabs__tab--soon")
+  trendsTabSoon.on("click", function() {
+    $('.trends-card').addClass("trends-card--hidden");
+    $(".trends-button").addClass("trends-button--hidden");
+    $(".best-card").removeClass("best-card--visible")
+    $(".soon-card").addClass("soon-card--visible")
+
+  // Меняются цвета табов при клике
+    $(".trends-tabs__tab--soon").addClass("soon-tab")
+    $(".trends-tabs__tab--primary").addClass("news-tab");
+    $(".trends-tabs__tab--best").removeClass("best-tab");
+  })
+
 })
